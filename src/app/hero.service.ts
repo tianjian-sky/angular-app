ae2b53259f4feb6e0e155e6d595be59b6cf9e0a4
tianjian_yu <532861047@qq.com>
2020-07-26 19:00:32 +0800

    service @Injectable

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+/**
+ * 组件不应该直接获取或保存数据，它们不应该了解是否在展示假数据。 它们应该聚焦于展示数据，而把数据访问的职责委托给某个服务。
+ * 
+ * 服务是在多个“互相不知道”的类之间共享信息的好办法。
+ */
+
+
+// @Injectable 装饰器 它把这个类标记为依赖注入系统的参与者之一
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes:Hero[] = HEROES;
-
+  heroes: Hero[];
   selectedHero: Hero;
 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
