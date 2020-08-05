db99921bb38c849961547f4a67114748121e82f6
lrz <lrz_china@163.com>
2020-08-05 12:49:12 +0800

    mock api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service'
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // http模块
 
 /**
  * 组件不应该直接获取或保存数据，它们不应该了解是否在展示假数据。 它们应该聚焦于展示数据，而把数据访问的职责委托给某个服务。
@@ -17,16 +19,40 @@ import { MessageService } from './message.service'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { } // 服务中的服务
+  constructor(private http: HttpClient, private messageService: MessageService) { } // 服务中的服务
+  
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)  // http请求
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
